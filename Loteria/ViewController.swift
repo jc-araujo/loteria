9dc1fb6aeabb4867f5b9b82291c05a1c98987807
João Araujo <eagle.araujo@icloud.com>
2021-11-07 17:02:31 -0300

    Initial Commit

---
+++ Loteria/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Loteria
+//
+//  Created by Murillo R. Araujo on 07/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
